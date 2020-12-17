d8f3be9df600bde5d1c59d24f4721d571829fcb2
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-17 17:07:23 -0500

    made middle earth

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -42,19 +42,27 @@ const makeMiddleEarth = () => {
   // console.log("Trying to make middle earth.");
 
   // 1. create a section tag with an id of middle-earth
+const $section = $("<section>")
+$section.attr("id","middle-earth")
 
   // 2. append the section to the body of the DOM.
-
+$("body").append($section)
   // 3. use a for loop to iterate over the lands array that does the following:
+for (let index = 0; index <lands.length; index++){
 
-  //   3a. creates an article tag (there should be one for each land when the loop is done)
+//   3a. creates an article tag (there should be one for each land when the loop is done)
+  const $article = $("<article>")
 
   //   3b. gives each land article an `id` tag of the corresponding land name
+  $article.attr("id", lands[index])
 
   //   3c. includes an h1 with the name of the land inside each land article
+  $article.html(`<h1>${lands[index]}</h1>`)
 
   //   3d. appends each land to the middle-earth section
-
+  $section.append($article)
+  
+  }
 };
 
 // COMMIT YOUR WORK
