2a09b71d59ef8cdbf73240ba567fc7b8b23e598e
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-17 20:59:47 -0500

    made the rind and gave it to frodo

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -111,11 +111,10 @@ const keepItSecretKeepItSafe = () => {
 
   // 2. add the ring as a child of Frodo
     // hint: Frodo does not have an id, but there is a command to retrieve all elements with a certain class. This should give you an array for you to access . . .
-  $(".hobbit")
+  
+    $div.appendTo($(".hobbit").eq(0))
 
-    hobbits.forEach((hobbit) => {
-      if 
-    })
+   
     // when you think you have given Frodo the ring, check in your Elements tab to see that it works correctly
 
 };
