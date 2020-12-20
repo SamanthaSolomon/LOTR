68ac567d4f3c3476893226f5a2f8584b1c29d2bd
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-20 18:24:24 -0500

    chap 9 updated gandalf

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -259,7 +259,12 @@ const theBalrog = () => {
 
   // 1. change the 'Gandalf' text to 'Gandalf the White'
 
+  $('.buddy').eq(0).text('Gandalf the White')
+
+
   // 2. add a class "the-white" to this element
+
+  $('.buddy').eq(0).removeClass('buddy').addClass('the-white')
 
   // 3. in the style.css file, add a css rule to make elements of the class "the-white" have a white background and a grey border
 
