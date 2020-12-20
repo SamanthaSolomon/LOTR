667d0272a9682d3de7e7c9f12f88b6c45db9bdde
Samantha Solomon <sleahsolomon@gmail.com>
2020-12-20 17:07:24 -0500

    chap 7 complete - strider to aragorn

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -209,6 +209,11 @@ const beautifulStranger = () => {
   // 1. change the buddy 'Strider' textnode to "Aragorn"
      // hint: You can get a list of elements by tag name, such as 'aside'
 
+     const $strider = $("aside ul").children("li").eq(3).text('Aragon')
+     console.log ("$strider", $strider)
+     
+
+
 };
 
 // COMMIT YOUR WORK
